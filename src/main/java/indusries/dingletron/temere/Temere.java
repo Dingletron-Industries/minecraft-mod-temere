3f1ac4e3b5c43a8a344e70a243493debb4b6db07
JordanPicton <jordan_picton@hotmail.com>
2026-03-01 12:30:50 +0000

    chore: small cleanup.

--- src/main/java/indusries/dingletron/temere/Temere.java
+++ src/main/java/indusries/dingletron/temere/Temere.java
@@ -3,7 +3,6 @@ package indusries.dingletron.temere;
 import indusries.dingletron.temere.block.ModBlocks;
 import indusries.dingletron.temere.item.ModCreativeModeTabs;
 import indusries.dingletron.temere.item.ModItems;
-import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -15,7 +14,6 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
@@ -26,7 +24,7 @@ public class Temere {
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public Temere(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
+        // Register the commonSetup method for mod loading
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in.
@@ -39,9 +37,6 @@ public class Temere {
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
 
-        // Register the item to a creative tab
-        //modEventBus.addListener(this::addCreative);
-
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
@@ -49,10 +44,6 @@ public class Temere {
     private void commonSetup(FMLCommonSetupEvent event) {
 
     }
-
-    // Add the example block item to the building blocks tab
-//    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-//    }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
