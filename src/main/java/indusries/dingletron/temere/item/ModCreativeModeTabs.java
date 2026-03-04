82380cab2d088ebc62d2438b28e36a1832713ae0
JordanPicton <jordan_picton@hotmail.com>
2026-03-04 07:27:12 +0000

    feat: add new creative tab & general cleaning.

--- src/main/java/indusries/dingletron/temere/item/ModCreativeModeTabs.java
+++ src/main/java/indusries/dingletron/temere/item/ModCreativeModeTabs.java
@@ -4,6 +4,7 @@ import indusries.dingletron.temere.Temere;
 import indusries.dingletron.temere.block.ModBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
@@ -15,13 +16,17 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Temere.MOD_ID);
 
-    public static final Supplier<CreativeModeTab> TEMERE_TAB = CREATIVE_MODE_TAB.register("temere_tab", () -> CreativeModeTab.builder()
-            .icon(() -> new ItemStack(ModBlocks.TIN_ORE.get())).title(Component.translatable("creative.temere"))
+    // Mod Items
+    public static final Supplier<CreativeModeTab> TEMERE_ITEM_TAB = CREATIVE_MODE_TAB.register("temere_item_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModItems.RAW_TIN.get())).title(Component.translatable("creativetab.temere.items"))
             .displayItems((itemDisplayParameters, output) -> {
                 // Misc Items
                 output.accept(ModItems.COAL_CHUNK);
                 output.accept(ModItems.FISH_BONES);
                 output.accept(ModItems.FOSSIL);
+                // Tin
+                output.accept(ModItems.RAW_TIN);
+                output.accept(ModItems.TIN_INGOT);
                 // Foods
 
                 // Fish
@@ -40,16 +45,21 @@ public class ModCreativeModeTabs {
                 output.accept(ModItems.SARDINE);
                 output.accept(ModItems.SEA_SNAIL);
                 output.accept(ModItems.TUNA);
+            })
+            .build());
 
+    // Mod Blocks
+    public static final Supplier<CreativeModeTab> TEMERE_BLOCK_TAB = CREATIVE_MODE_TAB.register("temere_block_tab", () -> CreativeModeTab.builder()
+            .icon(() -> new ItemStack(ModBlocks.TIN_ORE.get())).withTabsBefore(ResourceLocation.fromNamespaceAndPath(Temere.MOD_ID, "temere_item_tab")).title(Component.translatable("creativetab.temere.blocks"))
+            .displayItems((itemDisplayParameters, output) -> {
                 // Tin
-                output.accept(ModItems.RAW_TIN);
-                output.accept(ModItems.TIN_INGOT);
                 output.accept(ModBlocks.TIN_ORE);
                 output.accept(ModBlocks.DEEPSLATE_TIN_ORE);
                 output.accept(ModBlocks.RAW_TIN_BLOCK);
                 output.accept(ModBlocks.TIN_BLOCK);
             })
             .build());
+
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TAB.register(eventBus);
